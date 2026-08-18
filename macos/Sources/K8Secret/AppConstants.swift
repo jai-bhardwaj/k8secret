e7a33086a9f6019b68b3f868a1e824d4021dc9f7
Jai Bhardwaj <0987sujals@gmail.com>
2026-08-18 17:08:53 +0530

    release: bump to 0.6.11

--- macos/Sources/K8Secret/AppConstants.swift
+++ macos/Sources/K8Secret/AppConstants.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 enum AppConstants {
-    static let version = "0.6.10"
+    static let version = "0.6.11"
     static let appName = "K8Secret"
 
     // Auto-update: release manifest served from the repo via GitHub raw.
